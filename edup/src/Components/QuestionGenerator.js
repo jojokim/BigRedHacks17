ec2b39e5264b24f204c173af505f8603d6f629e3
jojokim <jojokim@umd.edu>
2017-09-17 12:13:59 +0900

    mui
    mui

--- edup/src/Components/QuestionGenerator.js
+++ edup/src/Components/QuestionGenerator.js
@@ -1,1 +1,8 @@
 import React from 'react';
+
+
+export default class questionGenerator extends Component {
+  render() {
+    return();
+  }
+}
--- edup/src/index.js
+++ edup/src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const Main = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
